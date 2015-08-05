a3b3f0c3fd4dabd8ccfd240e8945c82baa597bb2
Pyoungjin Kim <pyoungjin@pjhouse.com>
2015-08-05 14:25:34 +0900

    build > account UI 1차 완

--- resources/views/office/account.blade.php
+++ resources/views/office/account.blade.php
@@ -40,90 +40,94 @@
 			</ul>
 		</div>
 	</div>
-	<div class='row'>
-		<div class='span10 offset1'>
-			<form class='form-inline text-center' action="/{{Request::Path()}}/recoder" method="post">
-				<input type="hidden" name="_token" value="{{ csrf_token() }}"> 
-				<input type="hidden" name="date" value="{{ Request::input('date') }}"> 
-				<div class="input-prepend">
-					<span class="add-on">내용</span>
-					<input type="text" name="content" placeholder="ex) 101호 세입자 방세 납입">
-				</div>
-				<select class='span1' name='action'>
-					<option value="revenue">수입</option>
-					<option value="expense">지출</option>
-				</select>
-				<div class="input-append">
-					<input type="text" name="price" placeholder="100000">
-					<span class="add-on">원</span>
-				</div>
-				
-				<input class='btn btn-primary' type="submit" value="기록하기">
-			</form>
-			{{-- </div> --}}
-		</div>
-	</div>
 	
-	@if(count($ledger = OAccount::info('ledger')))
-	<div class='row'>
-		<div class='span10 offset1'>
-			<div class='text-right' style='padding:7px 10px;'>
-				<span>{{date('Y-m-d')}} 기준 - </span>
-				<span>수입:{{$revenue = OAccount::info('revenue')}} / </span>
-				<span>지출:{{$expense = OAccount::info('expense')}} / </span>
-				<span>합계:{{$amount = OAccount::info('amount')}}</span>
-			</div>
-		</div>
-	</div>
-	<div class='row'>
-		<div class='span10 offset1'>
-			<table class='table table-striped'>
-				<tr>
-					<td style='text-align: center; width:170px'>날짜</td>
-					<td style='text-align: center; '>내용</td>
-					<td style='text-align: center; width:80px;'>수입</td>
-					<td style='text-align: center; width:80px;'>지출</td>
-					<td style='text-align: center; width:100px;'>처리</td>
-				</tr>
-			@foreach ($ledger as $val) 
-				<tr>
-					<td style='text-align: center'>{{$val['date']}}</td>
-					<td style='text-align: left'>{{$val['content']}}</td>
-					@if($val['action'] == 'revenue')
-					<td style='text-align: right'>{{$val['price']}}</td>
-					<td></td>
-					@elseif($val['action'] == 'expense')
-					<td></td>
-					<td style='text-align: right'>{{$val['price']}}</td>
-					@endif
-					<td style='text-align: center'>{{$val['id']}}</td>
-				</tr>
-			@endforeach
-				<tr>
-					<td style='text-align: center'><h5>총계</h5></td>
-					<td ></td>
-					<td style='text-align: right'><h5>{{$revenue}} 원</h5></td>
-					<td style='text-align: right'><h5>{{$expense}} 원</h5></td>
-					<td style='text-align: center'><h5>{{$amount}} 원</h5></td>
-				</tr>
-			</table>
-		</div>
-	</div>
-	@else
-	<div class='row'>
-		<div class='span10 offset1'>
-			@if(OAccount::info('search_month') > date('Y-m') || OAccount::info('search_month') < '2015-06')
+	@if(OAccount::info('search_month') > date('Y-m') || OAccount::info('search_month') < '2015-06')
+		<div class='row'>
+			<div class='span10 offset1'>
 				<div class="alert alert-error">
 					{{-- <button type="button" class="close" data-dismiss="alert">&times;</button> --}}
 					<strong>에러!</strong> 비정상적인 접속입니다. 
 				</div>
-			@else
-				<div class="alert">
-					{{-- <button type="button" class="close" data-dismiss="alert">&times;</button> --}}
-					<strong>해당 자료가 없습니다.</strong>
-				</div>
-			@endif
+			</div>
 		</div>
-	</div>
+	@else
+		<div class='row'>
+			<div class='span10 offset1'>
+				<form class='form-inline text-center' action="/{{Request::Path()}}/recoder" method="post">
+					<input type="hidden" name="_token" value="{{ csrf_token() }}"> 
+					<input type="hidden" name="date" value="{{ Request::input('date') }}"> 
+					<div class="input-prepend">
+						<span class="add-on">내용</span>
+						<input type="text" name="content" placeholder="ex) 101호 세입자 방세 납입">
+					</div>
+					<select class='span1' name='action'>
+						<option value="revenue">수입</option>
+						<option value="expense">지출</option>
+					</select>
+					<div class="input-append">
+						<input type="text" name="price" placeholder="100000">
+						<span class="add-on">원</span>
+					</div>
+					
+					<input class='btn btn-primary' type="submit" value="기록하기">
+				</form>
+				{{-- </div> --}}
+			</div>
+		</div>
+
+		@if(count($ledger = OAccount::info('ledger')))
+		<div class='row'>
+			<div class='span10 offset1'>
+				<div class='text-right' style='padding:7px 10px;'>
+					<span>{{date('Y-m-d')}} 기준 - </span>
+					<span>수입:{{number_format($revenue = OAccount::info('revenue'))}} / </span>
+					<span>지출:{{number_format($expense = OAccount::info('expense'))}} / </span>
+					<span>합계:{{number_format($amount = OAccount::info('amount'))}}</span>
+				</div>
+			</div>
+		</div>
+		<div class='row'>
+			<div class='span10 offset1'>
+				<table class='table table-striped'>
+					<tr>
+						<td style='text-align: center; width:170px'>날짜</td>
+						<td style='text-align: center; '>내용</td>
+						<td style='text-align: center; width:80px;'>수입</td>
+						<td style='text-align: center; width:80px;'>지출</td>
+						<td style='text-align: center; width:100px;'>처리</td>
+					</tr>
+				@foreach ($ledger as $val) 
+					<tr>
+						<td style='text-align: center'>{{$val['date']}}</td>
+						<td style='text-align: left'>{{$val['content']}}</td>
+						@if($val['action'] == 'revenue')
+						<td style='text-align: right'>{{number_format($val['price'])}}</td>
+						<td></td>
+						@elseif($val['action'] == 'expense')
+						<td></td>
+						<td style='text-align: right'>{{number_format($val['price'])}}</td>
+						@endif
+						<td style='text-align: center'>{{$val['id']}}</td>
+					</tr>
+				@endforeach
+					<tr>
+						<td style='text-align: center'><h5>총계</h5></td>
+						<td ></td>
+						<td style='text-align: right'><h5>{{number_format($revenue)}} 원</h5></td>
+						<td style='text-align: right'><h5>{{number_format($expense)}} 원</h5></td>
+						<td style='text-align: center'><h5>{{number_format($amount)}} 원</h5></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		@else
+		<div class='row'>
+			<div class='span10 offset1'>
+					<div class="alert">
+						<strong>해당 자료가 없습니다.</strong>
+					</div>
+			</div>
+		</div>
+		@endif
 	@endif
 @stop
